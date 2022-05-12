ec4866e150efb5c717a7685410b3730e0f0bbae1
Michael Green <michaeldangreen91@gmail.com>
2022-05-12 01:55:09 -0400

    added comments

--- todo.js
+++ todo.js
@@ -1,19 +1,20 @@
 const todos = [''];
            
 render();
-
+// the above renders the empty array
 function addToDo() {
+    // adds items to the array
     const textbox = document.getElementById("todo-title");
     const title = textbox.value;
     todos.push(title);
 
     render();
 }
-
+// the above renders the array as things are added
 function render() {
     // reset the list
     document.getElementById('todo-list').innerHTML = '';
-
+// renders the list as things are entered into it
     todos.forEach(function (todoTitle) {
         const element = document.createElement('div');
         element.innerText = todoTitle;
@@ -22,9 +23,10 @@ function render() {
     });
     
 }
-
+// deletes the entire array
 function deleteToDo() {
     todos.length = 0;
 
     render();
+    // renders the deleted array
 }
